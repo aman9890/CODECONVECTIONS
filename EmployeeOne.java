03b98163797f1df2379cef5969dfa1c03b82c98f
aman9890 <79520184+aman9890@users.noreply.github.com>
2021-02-26 17:41:17 +0530

    file uploaded

---
+++ EmployeeOne.java
@@ -0,0 +1,29 @@
+public class EmployeeOne {
+
+    public class Employee
+    {
+        int id;
+        double salary=0.0;
+
+        public Employee(int Id) {
+            this.id = id;
+        }
+
+        public Employee() {
+        }
+
+        double calTax(){
+            if(this.salary > 500000.00 && this.salary < 1000000)
+                return this.salary*0.1;
+
+            else if(this.salary >1000000.00 && this.salary < 1500000)
+            return this.salary*0.2;
+
+        else
+            return this.salary*0.3;
+
+        }
+
+
+    }
+}
